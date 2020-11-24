0b5ae96525528f02f29cc77d617e6e7d979e330f
Daniel Gusenburger <danielgusenburger@online.de>
2020-11-24 17:35:10 +0100

    removed unnecessary files

--- TimeTravelDebugger.py
+++ TimeTravelDebugger.py
@@ -1,1 +1,10 @@
-import Debugger
+class TimeTravelDebugger(Tracer):
+
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, *args, **kwargs):
+        pass
