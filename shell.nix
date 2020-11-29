058a3aa0f7b529918faee0134a278edf0da1ec60
Daniel Tabellion <wellos94@web.de>
2020-11-29 17:21:59 +0100

    nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,16 @@
 let
   sources = import nix/sources.nix;
   pkgs = import sources.nixpkgs {};
+  mach-nix = import sources.mach-nix {};
+  py = mach-nix.mkPython {
+    # replace with mkPythonShell if shell is wanted
+    requirements = builtins.readFile ./requirements.txt;
+  };
 in
 
 pkgs.mkShell {
-  buildInputs = with pkgs;[
-    jupyter
+  buildInputs = [
+    py
+    pkgs.jupyter
   ];
 }
