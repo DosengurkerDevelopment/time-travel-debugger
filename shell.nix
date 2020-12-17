6637c306433b8273df31effacb09253036672df8
Daniel Tabellion <wellos94@web.de>
2020-12-17 19:21:20 +0100

    nix and requirements

--- shell.nix
+++ shell.nix
@@ -17,6 +17,8 @@ let
       kernels = [ iPython ];
       extraPackages = _: [
         unstable.python37Packages.ipython
+        unstable.python37Packages.lxml
+        unstable.python37Packages.cssselect
         unstable.python37Packages.colorama
         unstable.python37Packages.ipywidgets
         unstable.python37Packages.ipykernel
@@ -29,6 +31,8 @@ pkgs.mkShell {
   buildInputs = [
     py
     unstable.python37Packages.ipython
+    unstable.python37Packages.lxml
+    unstable.python37Packages.cssselect
     unstable.python37Packages.colorama
     unstable.python37Packages.ipywidgets
     unstable.python37Packages.ipykernel
