1527a45c2270a56eaaea6905388f47dab44d3453
Daniel Gusenburger <danielgusenburger@online.de>
2020-12-18 20:22:37 +0100

    save

--- time_travel_debugger/model/event.py
+++ time_travel_debugger/model/event.py
@@ -1,23 +1,26 @@
-from  enum import Enum 
+from enum import Enum
+import os
+
 
 class EventType(Enum):
     VAR_CHANGE = "var_change"
     FUNC_CALL = "func_call"
     BREAK_HIT = "break_hit"
 
-class Event(object):
-    """ class for storing information about events happening during program
-    execution """ 
 
-    def __init__(self, type:EventType, id, line, func, file, exec_point):
+class Event(object):
+    """class for storing information about events happening during program
+    execution"""
+
+    def __init__(self, type: EventType, id, line, func, file, exec_point):
         self.id = id
         self.type = type
         self.line = line
         self.func = func
         self.file = file
         self.exec_point = exec_point
-    
+
     def __str__(self):
-        return f"<type: {self.type}>: id:{self.id}, line:{self.line}, func:{self.func}, file:{self.file}\n"
+        return f"{os.path.basename(self.file)}:{self.func}:{self.line} -- {self.type}"
 
     __repr__ = __str__
