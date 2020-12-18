ee92d63fa943b9f14061bcf8f5758c383cf2ddb3
Daniel Tabellion <wellos94@web.de>
2020-12-18 19:31:52 +0100

    added exec_point to events

--- time_travel_debugger/model/event.py
+++ time_travel_debugger/model/event.py
@@ -9,14 +9,15 @@ class Event(object):
     """ class for storing information about events happening during program
     execution """ 
 
-    def __init__(self, type:EventType, id, line, func, file):
+    def __init__(self, type:EventType, id, line, func, file, exec_point):
         self.id = id
         self.type = type
         self.line = line
         self.func = func
         self.file = file
+        self.exec_point = exec_point
     
     def __str__(self):
-        return f"<type: {self.type}, id:{self.id}, line:{self.line}, func:{self.func}, file:{self.file}\n"
+        return f"<type: {self.type}>: id:{self.id}, line:{self.line}, func:{self.func}, file:{self.file}\n"
 
     __repr__ = __str__
